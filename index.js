e2bfac7c5a6b884e23f359cf159172dcd0f1f987
Raymond Hsu <raymondhsu@duck.com>
2023-06-24 23:39:54 +0800

    init upload

---
+++ index.js
@@ -0,0 +1,81 @@
+const { WebhookClient } = require('discord.js');
+const al = require('@dumpy/andylib');
+const l = new al.logger()
+require('dotenv').config()
+const config = require('./config.json')
+const client = new WebhookClient({ id: process.env.ID, token: process.env.TOKEN });
+// import puppeteer
+const puppeteer = require('puppeteer');
+// scrape https://www.gsmarena.com/news.php3
+async function fetch() {
+    // launch puppeteer
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(config.link);
+    // get all the news titles
+    const titles = await page.evaluate(() => {
+        const news = document.querySelectorAll('.news-item');
+        const titles = [];
+        news.forEach((item) => {
+            titles.push(item.querySelector('h3').innerText);
+        })
+        return titles
+    });
+    const bodys = await page.evaluate(() => {
+        const news = document.querySelectorAll('.news-item');
+        const bodys = [];
+        news.forEach((item) => {
+            bodys.push(item.querySelector('p').innerText);
+        })
+        return bodys
+    })
+    const links = await page.evaluate(() => {
+        const news = document.querySelectorAll('.news-item');
+        const links = [];
+        news.forEach((item) => {
+            links.push(item.querySelector('a').href);
+        })
+        return links
+    })
+    const realme = titles.filter((title) => { return title.includes('Realme') })
+    console.log(realme);
+    // get the links for the realme
+    const realmeLinks = links.filter((link) => { return link.includes('realme') })
+    console.log(realmeLinks);
+    await browser.close();
+    return realmeLinks;
+}
+let oldLinks = [];
+ l.info('Starting');
+    fetch().then((links) => {
+        // check if there are any new links
+        const newLinks = links.filter((link) => !oldLinks.includes(link));
+        if (newLinks.length > 0) {
+            // send the new links
+            client.send(`${newLinks.join(' ')}`);
+            l.debug('Sent new links');
+        } else {
+            l.error("No new links");
+            return;
+        }
+        // set the old links to the new links
+        oldLinks = links;
+    }).catch((err) => console.log(err));
+
+setInterval(() => {
+    l.info('Starting');
+    fetch().then((links) => {
+        // check if there are any new links
+        const newLinks = links.filter((link) => !oldLinks.includes(link));
+        if (newLinks.length > 0) {
+            // send the new links
+            client.send(`${newLinks.join(' ')}`);
+            l.debug('Sent new links');
+        } else {
+            l.error("No new links");
+            return;
+        }
+        // set the old links to the new links
+        oldLinks = links;
+    }).catch((err) => console.log(err));
+}, 900000)
